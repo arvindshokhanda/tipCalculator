6cc7a2a72822903daf6ebda85b8665161085db53
arvindshokhanda <arvindshokhanda21@gmail.com>
2020-11-07 00:47:25 +0530

    final commit
    final commit
    added oops, exceptional handling

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tipCalculator
 This a type of calculator in which you can calculate the tip per head and the total amount per person.
+
+Steps to use tipCalculator:
+1. Fill all the(three) input fields with valid input values.
+2. Click on "Calculate" button.
+3. Click on "Reset" button to reset the calculator.
+![screenshot](https://github.com/arvindshokhanda/tipCalculator/image.png?raw=true)
--- tip_calculator.js
+++ tip_calculator.js
@@ -1,31 +1,76 @@
-function calculateTip() {
-  try {
+"use strict";
+// Logical calculation functioning code
+var CalculateTip = {
+    tipPerHead: function(billAmount, tipPercent, numOfPeople) {
+        return (((billAmount * tipPercent) / 100) / numOfPeople).toFixed(2).toString();
+    },
+    totalBill: function(billAmount, tipPercent, numOfPeople) {
+        return ((((billAmount * tipPercent) / 100) / numOfPeople) + (billAmount / numOfPeople)).toFixed(2).toString();
+    },
+};
 
-    let billAmount = document.getElementById("billAmount").valueAsNumber;
-    let tipPercent = document.getElementById("tipPercent").valueAsNumber;
-    let numOfPeople = document.getElementById("numOfPeople").valueAsNumber;
-    if(billAmount < 0) {
-      throw new Error("Bill should be greater than 0");
-    } else if(tipPercent < 0) {
-      throw new Error("Tip should be greater than 0");
-    } else if(numOfPeople < 0 || numOfPeople%1 != 0) {
-      throw new Error("No. of people must be a natural no.");
+// This part manages events and *uses* the operations.
+(function() {
+
+    var calculate = document.getElementById('calculateBtn');
+    var resetbutton = document.getElementById('resetBtn');
+    var tipPerHead = document.getElementById('tipPerHead');
+    var totalBillPerHead = document.getElementById('totalBillPerHead');
+
+    function setTipPerHead(value) {
+        tipPerHead.innerHTML = '$'+value;
     }
-    
-    document.getElementById("tipPerHead").innerHTML =
-     "<h1>$" + (((billAmount * tipPercent) / 100) / numOfPeople).toFixed(2).toString() + "</h1>";
-    document.getElementById("totalBillPerHead").innerHTML =
-     "<h1>$" + ((((billAmount * tipPercent) / 100) / numOfPeople) 
-     + (billAmount / numOfPeople)).toFixed(2).toString() + "</h1>";
-      
-      
-  } catch(err) {
-    alert(err);
-  }
-  return false;
-}
-function resetfn() {
-  document.getElementById("tipCalculator").reset();
-  document.getElementById("tipPerHead").innerHTML = "<h1>$</h1>";
-  document.getElementById("totalBillPerHead").innerHTML = "<h1>$</h1>";
-}   
+
+    function setTotalBill(value) {
+        totalBillPerHead.innerHTML = '$'+value;
+    }
+
+    // Calculation form submit event handler
+    calculate.addEventListener('click', function() {
+        try{
+            var billAmount = document.getElementById("billAmount").value;
+            var tipPercent = document.getElementById("tipPercent").value;
+            var numOfPeople = document.getElementById("numOfPeople").value;
+            
+            if (billAmount=='' && tipPercent=='' && numOfPeople=='') {
+                throw new Error("Fill all the fields!");
+            } else if(billAmount=='') {
+                throw new Error("Fill Bill field!")
+            } else if(tipPercent=='') {
+                throw new Error("Fill Tip field!")
+            } else if(numOfPeople=='') {
+                throw new Error("Fill Number of People field!")
+            } else if (billAmount < 0) {
+                throw new Error("Bill should be greater than 0!");
+            } else if (tipPercent < 0) {
+                throw new Error("Tip should be greater than 0!");
+            } else if (numOfPeople < 1 || numOfPeople % 1 != 0) {
+                throw new Error("Invalid number of people!");
+            } else {
+                setTipPerHead(CalculateTip.tipPerHead(billAmount, tipPercent, numOfPeople));
+                setTotalBill(CalculateTip.totalBill(billAmount, tipPercent, numOfPeople));
+            }
+        } catch(err) {
+            alert(err);
+        }
+        return false;
+    });
+
+    // Reset form event handler
+    resetbutton.addEventListener('click', function() {
+        try{
+            var billAmount = document.getElementById("billAmount");
+            var tipPercent = document.getElementById("tipPercent");
+            var numOfPeople = document.getElementById("numOfPeople");
+
+            billAmount.value = '';
+            tipPercent.value = '';
+            numOfPeople.value = '';
+
+            tipPerHead.innerHTML = '$0.00';
+            totalBillPerHead.innerHTML = '$0.00';
+        } catch(err) {
+            console.log(err);
+        }
+    });
+}()); 
